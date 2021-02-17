fb821c2e5df75096b31b2d460198b1ee6555b5db
ALIENWARE <ALIENWARE@DESKTOP-JPUEAOP>
2021-02-17 17:39:34 +0300

    First product

---
+++ CampIntro1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CampIntro1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //Do not repeat yourself -  Kendini tekrarlama
+            //değer tutucu, alias
+
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 3200;
+            double faizOranı = 1.45;
+            bool sistemeGrisYapmisMi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+            if ( sistemeGrisYapmisMi ==  true)
+            {
+                Console.WriteLine("Kulanıcı Ayrları Butonu");
+
+
+
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+           Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
---
+++ Donguler/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {    //i++ iden başla birer birer arttır 
+        static void Main(string[] args)
+        {
+            string kurs1 = "yazılımcı geliştirme kampı";
+            string kurs2 = "programlamaya giriş";
+            string kurs3 = "java";
+            string kurs4 = "python";
+
+            //array - dizi
+
+            string[ ] kurslar = new string[] { "yazılımcı geliştirme kampı", "programlamaya giriş",
+                "java", "python", "c#" };
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]); 
+            }
+
+            Console.WriteLine("for bitti");
+
+            //aşağıdaki kurs her bir elemanı dolaşırken verdiğimiz takma isim
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("sayfa sonu - footer");
+        }
+    }
+}
