139ef24b22d0081f5765ce3fb0183796bb8faa8b
ALIENWARE <ALIENWARE@DESKTOP-JPUEAOP>
2021-02-27 01:04:42 +0300

    1 p.m

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ctrl+K ctrl+c toplu yorum satırı yapma 
+            //ctrl+k ctrl+u geri cevirme 
+            //string[] isimler = new string[] { " Engin ", "Murat", "Kerem", "Halil" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+            //Console.WriteLine(isimler[4]);
+
+
+            List<string> isimler2 = new List<string> {"Engin","Murat","Kerem","Halil"};
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+            
+
+        }
+    }
+}
